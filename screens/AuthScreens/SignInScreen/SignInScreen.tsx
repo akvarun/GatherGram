ec2a83f977adb42de4c554aec5501e7c97250a12
Varunak08 <akvarun08@gmail.com>
2023-12-01 11:53:10 +0530

    Removed SocialSignUp and Updated the logo

--- screens/AuthScreens/SignInScreen/SignInScreen.tsx
+++ screens/AuthScreens/SignInScreen/SignInScreen.tsx
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
-import {
-  View,
-  Image,
-  StyleSheet,
-  useWindowDimensions,
-  ScrollView,
-  Alert,
-} from "react-native";
-import Logo from "./logo.png";
+import { View, Image, StyleSheet, ScrollView, Alert } from "react-native";
+import Logo from "./Gather.png";
 import CustomInput from "../components/CustomInput";
 import CustomButton from "../components/CustomButton";
-import SocialSignInButtons from "../components/SocialSignInButtons";
 import { useNavigation } from "@react-navigation/native";
 import { useForm } from "react-hook-form";
 import { useSignInEmailPassword } from "@nhost/react";
@@ -40,7 +32,7 @@ const SignInScreen = () => {
     }
 
     if (needsEmailVerification) {
-      Alert.alert("Verify you email", "Check your email and follow the link");
+      Alert.alert("Verify your email", "Check your email and follow the link");
     }
   };
 
@@ -53,68 +45,69 @@ const SignInScreen = () => {
   };
 
   return (
-    <ScrollView showsVerticalScrollIndicator={false} style={styles.root}>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      showsVerticalScrollIndicator={false}
+    >
       <Image source={Logo} style={styles.logo} resizeMode="cover" />
-      <View style={styles.container}>
-        <CustomInput
-          name="email"
-          placeholder="Email"
-          control={control}
-          rules={{
-            required: "Email is required",
-            pattern: { value: EMAIL_REGEX, message: "Email is invalid" },
-          }}
-        />
 
-        <CustomInput
-          name="password"
-          placeholder="Password"
-          secureTextEntry
-          control={control}
-          rules={{
-            required: "Password is required",
-            minLength: {
-              value: 3,
-              message: "Password should be minimum 3 characters long",
-            },
-          }}
-        />
+      <CustomInput
+        name="email"
+        placeholder="Email"
+        control={control}
+        rules={{
+          required: "Email is required",
+          pattern: { value: EMAIL_REGEX, message: "Email is invalid" },
+        }}
+      />
 
-        <CustomButton
-          text={isLoading ? "Loading..." : "Sign In"}
-          onPress={handleSubmit(onSignInPressed)}
-        />
+      <CustomInput
+        name="password"
+        placeholder="Password"
+        secureTextEntry
+        control={control}
+        rules={{
+          required: "Password is required",
+          minLength: {
+            value: 3,
+            message: "Password should be minimum 3 characters long",
+          },
+        }}
+      />
 
-        <CustomButton
-          text="Forgot password?"
-          onPress={onForgotPasswordPressed}
-          type="TERTIARY"
-        />
+      <CustomButton
+        text={isLoading ? "Loading..." : "Sign In"}
+        onPress={handleSubmit(onSignInPressed)}
+      />
 
-        <SocialSignInButtons />
+      <CustomButton
+        text="Forgot password?"
+        onPress={onForgotPasswordPressed}
+        type="TERTIARY"
+      />
 
-        <CustomButton
-          text="Don't have an account? Create one"
-          onPress={onSignUpPress}
-          type="TERTIARY"
-        />
-      </View>
+      <CustomButton
+        text="Don't have an account? Create one"
+        onPress={onSignUpPress}
+        type="TERTIARY"
+      />
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  root: {
-    backgroundColor: "white",
-    minHeight: "100%",
-  },
   container: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
     padding: 20,
   },
   logo: {
     width: "100%",
     height: undefined,
     aspectRatio: 16 / 9,
+    marginBottom: 20,
   },
 });
 
